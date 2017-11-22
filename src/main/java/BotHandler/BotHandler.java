e9e6786ba4b2d0f3b9dbf3564a7262c51fcd6362
Koroed022 <koroed022@mail.ru>
2017-11-22 17:21:12 +0700

    Forker v1.1

--- src/main/java/BotHandler/BotHandler.java
+++ src/main/java/BotHandler/BotHandler.java
@@ -1,19 +1,16 @@
 package BotHandler;
-
-
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
-import sun.java2d.loops.XORComposite;
+
 
 import java.util.ArrayList;
 
 public class BotHandler extends TelegramLongPollingBot{
     
-    public static final String TOKEN = "499399704:AAF2N5zGDcM48Qv-CXRaNiqvc-83Lk3Z9jM";
-    public static final String USERNAME = "fork_stavka_bot";
+
 
     public void onUpdateReceived(Update update) {
         try {
@@ -73,10 +70,10 @@ public class BotHandler extends TelegramLongPollingBot{
     }
 
     public String getBotUsername() {
-        return USERNAME;
+        return Token.USERNAME;
     }
 
     public String getBotToken() {
-        return TOKEN;
+        return Token.TOKEN;
     }
 }
---
+++ src/main/java/TeleBot.java
@@ -0,0 +1,32 @@
+import BotHandler.BotHandler;
+
+import com.spetcnaz.Bot;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class TeleBot {
+    private static TeleBot ourInstance = new TeleBot();
+    private static BotHandler botHandler;
+
+    public static TeleBot getInstance() {
+        return ourInstance;
+    }
+
+    private TeleBot() {
+        ApiContextInitializer.init();
+
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        botHandler = new BotHandler();
+        try {
+            botHandler.clearWebhook();
+            telegramBotsApi.registerBot(new BotHandler());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static BotHandler getBotHandler() {
+        return botHandler;
+    }
+}
